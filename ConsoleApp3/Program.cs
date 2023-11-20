308ba7c165cee6de6d1d584016aec13367f11984
Dando1337 <hmelevdaniil96@gmail.com>
2023-11-20 08:21:33 -0800

    Ya chto-to pomeにゃl_3.0

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,12 +9,16 @@ Console.WriteLine();
 double answer = 1;
 // Переменная для минимального числа
 double min = 0;
+// Переменные для нахождения F и F2
 double f;
 double f2;
+// Первая и вторая переменная(надо было назвать firstVariable и secondVariable)
 double x0 = 0;
 double x1 = 0;
+// Формулы
 f = Math.Pow(2, a) - 2 * Math.Cos(a);
 f2 = 2 * Math.Cos(a) + Math.Pow(Math.Log(2), 2) * Math.Pow(2, a);
+// Первое ветвление
 if (f < 0 && f2 < 0 || f > 0 && f2 > 0)
 {
     x0 = a;
@@ -22,12 +26,15 @@ if (f < 0 && f2 < 0 || f > 0 && f2 > 0)
 }
 f = Math.Pow(2, b) - 2 * Math.Cos(b);
 f2 = 2 * Math.Cos(b) + Math.Pow(Math.Log(2), 2) * Math.Pow(2, b);
+// Второе ветвление
 if (f < 0 && f2 < 0 || f > 0 && f2 > 0)
 {
     x0 = b;
     min = Math.Abs(2 * Math.Sin(a) + Math.Log(2) * Math.Pow(2, a));
 }
+// Вывод на экран
 Console.WriteLine("Метод хорд");
+// Цикл
 while (answer > 0.0001)
 {
     x1 = x0 - ((Math.Pow(2, x0) - 2 * Math.Cos(x0)) / (2 * Math.Sin(x0) + Math.Log(2) * Math.Pow(2, x0)));
@@ -38,9 +45,11 @@ while (answer > 0.0001)
     Console.WriteLine("|2^" + x1 + " - 2 * Cos(" + x1 + ")| = " + answer);
     x0 = x1;
 }
+// Вывод на экран
 Console.WriteLine("Ответ: " + x1);
 Console.WriteLine("Метод касательных");
 answer = 1;
+// Цикл
 while (answer > 0.0001)
 {
     if (a > b)
@@ -57,4 +66,5 @@ while (answer > 0.0001)
     Console.WriteLine("Точность");
     Console.WriteLine("|2^" + x1 + " - 2 * Cos(" + x1 + ") = " + answer);
 }
+// Вывод на экран
 Console.WriteLine("Ответ: " + x1);
